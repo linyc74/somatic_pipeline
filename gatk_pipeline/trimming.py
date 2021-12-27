831ef14b729111919713c3457b655b8566d69f6b
Yu-Cheng Lin <linyc74@MacAir.local>
2021-12-27 09:45:26 +0800

    trim-galore

--- gatk_pipeline/trimming.py
+++ gatk_pipeline/trimming.py
@@ -1,10 +1,69 @@
+from typing import Tuple
+from os.path import basename
+from .constant import CMD_LINEBREAK
 from .template import Processor, Settings
 
 
 class TrimGalore(Processor):
 
+    QUALITY = 20
+    LENGTH = 20
+    MAX_N = 0
+
+    fq1: str
+    fq2: str
+
+    out_fq1: str
+    out_fq2: str
+
     def __init__(self, settings: Settings):
         super().__init__(settings=settings)
 
-    def main(self):
-        pass
+    def main(self, fq1: str, fq2: str) -> Tuple[str, str]:
+        self.fq1 = fq1
+        self.fq2 = fq2
+
+        self.execute()
+        self.set_out_fq1()
+        self.set_out_fq2()
+
+        return self.out_fq1, self.out_fq2
+
+    def execute(self):
+        cmd = CMD_LINEBREAK.join([
+            'trim_galore',
+            '--paired',
+            f'--quality {self.QUALITY}',
+            '--phred33',
+            '--fastqc',
+            '--illumina',
+            f'--length {self.LENGTH}',
+            f'--max_n {self.MAX_N}',
+            '--trim-n',
+            '--gzip',
+            f'--output_dir {self.workdir}',
+            self.fq1,
+            self.fq2,
+        ])
+        self.call(cmd)
+
+    def set_out_fq1(self):
+        f = basename(self.fq1)
+        f = self.__strip_file_extension(f)
+        self.out_fq1 = f'{self.workdir}/{f}_val_1.fq.gz'
+
+    def set_out_fq2(self):
+        f = basename(self.fq2)
+        f = self.__strip_file_extension(f)
+        self.out_fq2 = f'{self.workdir}/{f}_val_2.fq.gz'
+
+    def __strip_file_extension(self, f):
+        for suffix in [
+            '.fq',
+            '.fq.gz',
+            '.fastq',
+            '.fastq.gz',
+        ]:
+            if f.endswith(suffix):
+                f = f[:-len(suffix)]  # strip suffix
+        return f
--- test/test_me.py
+++ test/test_me.py
@@ -11,5 +11,15 @@ class MyTest(TestCase):
         self.tear_down()
 
     def test_me(self):
-        TrimGalore(self.settings).main(
+        fq1, fq2 = TrimGalore(self.settings).main(
+            fq1=f'{self.indir}/normal.1.fq.gz',
+            fq2=f'{self.indir}/normal.2.fq.gz'
+        )
+        self.assertFileExists(
+            expected=f'{self.workdir}/normal.1_val_1.fq.gz',
+            actual=fq1
+        )
+        self.assertFileExists(
+            expected=f'{self.workdir}/normal.2_val_2.fq.gz',
+            actual=fq2
         )
