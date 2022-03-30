cb3d292ad608e9492548e473c70640d4e8fa1ca8
linyc74 <yclin.python@gmail.com>
2022-03-30 17:01:30 +0800

    move snpeff summary files

--- somatic_pipeline/annotation.py
+++ somatic_pipeline/annotation.py
@@ -1,24 +1,25 @@
+import os
 from .template import Processor
 
 
 class SnpEff(Processor):
 
     GENOME_ID = 'GRCh38.99'
+    SUMMARY_PREFIX = 'snpeff-summary'
 
     vcf: str
     annotated_vcf: str
 
     def main(self, vcf: str) -> str:
         self.vcf = vcf
-
         self.execute()
-
+        self.move_summary_files()
         return self.annotated_vcf
 
     def execute(self):
         self.annotated_vcf = f'{self.outdir}/annotated.vcf'
-        html = f'{self.outdir}/snpEff-summary.html'
-        stderr = f'{self.outdir}/snpEff.log'
+        html = f'{self.outdir}/{self.SUMMARY_PREFIX}.html'
+        stderr = f'{self.outdir}/snpeff.log'
 
         cmd = self.CMD_LINEBREAK.join([
             'snpeff',
@@ -31,3 +32,8 @@ class SnpEff(Processor):
         ])
 
         self.call(cmd)
+
+    def move_summary_files(self):
+        dstdir = f'{self.outdir}/snpeff'
+        os.makedirs(dstdir, exist_ok=True)
+        self.call(f'mv {self.outdir}/{self.SUMMARY_PREFIX}* {dstdir}/')
