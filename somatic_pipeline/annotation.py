b58877b0192d24a137a75901ca95ee8168a18ab9
linyc74 <yclin.python@gmail.com>
2022-04-15 12:02:33 +0800

    add snpsift dbNSFP

--- somatic_pipeline/annotation.py
+++ somatic_pipeline/annotation.py
@@ -1,5 +1,38 @@
 import os
+from typing import Optional
+from .tools import edit_fpath
 from .template import Processor
+
+
+class Annotation(Processor):
+
+    vcf: str
+    snpsift_dbnsfp_txt_gz: Optional[str]
+
+    def main(
+            self,
+            vcf: str,
+            snpsift_dbnsfp_txt_gz: Optional[str]) -> str:
+        self.vcf = vcf
+        self.snpsift_dbnsfp_txt_gz = snpsift_dbnsfp_txt_gz
+        self.run_snpeff()
+        self.run_snpsift()
+        self.move_vcf()
+        return self.vcf
+
+    def run_snpeff(self):
+        self.vcf = SnpEff(self.settings).main(vcf=self.vcf)
+
+    def run_snpsift(self):
+        if self.snpsift_dbnsfp_txt_gz is not None:
+            self.vcf = SnpSift(self.settings).main(
+                vcf=self.vcf,
+                dbnsfp_txt_gz=self.snpsift_dbnsfp_txt_gz)
+
+    def move_vcf(self):
+        dst = f'{self.outdir}/annotated.vcf'
+        self.call(f'mv {self.vcf} {dst}')
+        self.vcf = dst
 
 
 class SnpEff(Processor):
@@ -8,32 +41,92 @@ class SnpEff(Processor):
     SUMMARY_PREFIX = 'snpeff-summary'
 
     vcf: str
-    annotated_vcf: str
+    output_vcf: str
 
     def main(self, vcf: str) -> str:
         self.vcf = vcf
+        self.set_output_vcf()
         self.execute()
         self.move_summary_files()
-        return self.annotated_vcf
+        return self.output_vcf
+
+    def set_output_vcf(self):
+        self.output_vcf = edit_fpath(
+            fpath=self.vcf,
+            old_suffix='.vcf',
+            new_suffix='-snpeff.vcf',
+            dstdir=self.workdir)
 
     def execute(self):
-        self.annotated_vcf = f'{self.outdir}/annotated.vcf'
         html = f'{self.outdir}/{self.SUMMARY_PREFIX}.html'
         stderr = f'{self.outdir}/snpeff.log'
-
         cmd = self.CMD_LINEBREAK.join([
             'snpeff',
             '-verbose',
             f'-htmlStats {html}',
             self.GENOME_ID,
             self.vcf,
-            f'> {self.annotated_vcf}',
+            f'> {self.output_vcf}',
             f'2> {stderr}',
         ])
-
         self.call(cmd)
 
     def move_summary_files(self):
         dstdir = f'{self.outdir}/snpeff'
         os.makedirs(dstdir, exist_ok=True)
         self.call(f'mv {self.outdir}/{self.SUMMARY_PREFIX}* {dstdir}/')
+
+
+class SnpSift(Processor):
+
+    vcf: str
+    dbnsfp_txt_gz: str
+
+    output_vcf: str
+
+    def main(
+            self,
+            vcf: str,
+            dbnsfp_txt_gz: str) -> str:
+        self.vcf = vcf
+        self.dbnsfp_txt_gz = dbnsfp_txt_gz
+
+        self.prepare_dbnsfp_txt()
+        self.set_output_vcf()
+        self.execute()
+
+        return self.output_vcf
+
+    def prepare_dbnsfp_txt(self):
+        self.__copy()
+        self.__index()
+
+    def __copy(self):
+        src = self.dbnsfp_txt_gz
+        dst = edit_fpath(
+            fpath=self.dbnsfp_txt_gz,
+            dstdir=self.workdir)
+        self.call(f'cp {src} {dst}')
+        self.dbnsfp_txt_gz = dst
+
+    def __index(self):
+        cmd = f'tabix -p vcf {self.dbnsfp_txt_gz}'
+        self.call(cmd)
+
+    def set_output_vcf(self):
+        self.output_vcf = edit_fpath(
+            fpath=self.vcf,
+            old_suffix='.vcf',
+            new_suffix='-snpsift.vcf',
+            dstdir=self.workdir)
+
+    def execute(self):
+        stderr = f'{self.outdir}/snpsift.log'
+        cmd = self.CMD_LINEBREAK.join([
+            'snpsift dbnsfp',
+            f'-db {self.dbnsfp_txt_gz}',
+            self.vcf,
+            f'> {self.output_vcf}',
+            f'2> {stderr}',
+        ])
+        self.call(cmd)
--- test/test_annotation.py
+++ test/test_annotation.py
@@ -1,5 +1,22 @@
-from somatic_pipeline.annotation import SnpEff
+from somatic_pipeline.annotation import Annotation, SnpEff, SnpSift
 from .setup import TestCase
+
+
+class TestAnnotation(TestCase):
+
+    def setUp(self):
+        self.set_up(py_path=__file__)
+
+    def tearDown(self):
+        self.tear_down()
+
+    def test_main(self):
+        actual = Annotation(self.settings).main(
+            vcf=f'{self.indir}/raw.vcf',
+            snpsift_dbnsfp_txt_gz=f'{self.indir}/22_0414_dbNSFP_chr9.txt.gz',
+        )
+        expected = f'{self.outdir}/annotated.vcf'
+        self.assertFileExists(expected, actual)
 
 
 class TestSnpEff(TestCase):
@@ -14,5 +31,22 @@ class TestSnpEff(TestCase):
         actual = SnpEff(self.settings).main(
             vcf=f'{self.indir}/raw.vcf'
         )
-        expected = f'{self.outdir}/annotated.vcf'
+        expected = f'{self.workdir}/raw-snpeff.vcf'
+        self.assertFileExists(expected, actual)
+
+
+class TestSnpSift(TestCase):
+
+    def setUp(self):
+        self.set_up(py_path=__file__)
+
+    def tearDown(self):
+        self.tear_down()
+
+    def test_main(self):
+        actual = SnpSift(self.settings).main(
+            vcf=f'{self.indir}/raw.vcf',
+            dbnsfp_txt_gz=f'{self.indir}/22_0414_dbNSFP_chr9.txt.gz'
+        )
+        expected = f'{self.workdir}/raw-snpsift.vcf'
         self.assertFileExists(expected, actual)
