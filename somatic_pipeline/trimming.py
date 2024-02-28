cbf71fb907c758bfa45f3bbec3eef9d71a6b189e
linyc74 <ylin@nycu.edu.tw>
2024-02-28 16:42:06 +0800

    move umi remover to trimming

--- somatic_pipeline/trimming.py
+++ somatic_pipeline/trimming.py
@@ -1,6 +1,8 @@
 import os
-from typing import Tuple, Optional
+import gzip
+from typing import Tuple, Optional, IO
 from os.path import basename
+from .tools import rev_comp
 from .template import Processor
 
 
@@ -142,3 +144,107 @@ class TrimGalore(Processor):
             if f.endswith(suffix):
                 f = f[:-len(suffix)]  # strip suffix
         return f
+
+
+class RemoveUmiAndAdapter(Processor):
+
+    fq1: str
+    fq2: str
+    umi_length: int
+    gz: bool
+
+    out_fq1: str
+    out_fq2: str
+
+    fq1_reader: IO
+    fq2_reader: IO
+    fq1_writer: IO
+    fq2_writer: IO
+
+    def main(self, fq1: str, fq2: str, umi_length: int, gz: bool) -> Tuple[str, str]:
+
+        self.fq1 = fq1
+        self.fq2 = fq2
+        self.umi_length = umi_length
+        self.gz = gz
+
+        self.open_files()
+
+        self.logger.info(f'Start removing UMI and universal adapters')
+
+        total_1, total_2, remain_1, remain_2 = 0, 0, 0, 0
+        while True:
+            header1 = self.fq1_reader.readline().strip()
+            header2 = self.fq2_reader.readline().strip()
+
+            if header1 == '':  # end of file
+                break
+
+            assert header1.split()[0] == header2.split()[0]
+
+            seq1 = self.fq1_reader.readline().strip()[self.umi_length:]
+            seq2 = self.fq2_reader.readline().strip()[self.umi_length:]
+            new_seq2 = strip_mate_3prime_umi(read=seq1, mate=seq2)
+            new_seq1 = strip_mate_3prime_umi(read=seq2, mate=seq1)
+            total_1 += len(seq1)
+            total_2 += len(seq2)
+            remain_1 += len(new_seq1)
+            remain_2 += len(new_seq2)
+
+            self.fq1_reader.readline()  # skip the '+' line
+            self.fq2_reader.readline()
+
+            qual1 = self.fq1_reader.readline().strip()
+            qual2 = self.fq2_reader.readline().strip()
+            u = self.umi_length
+            qual1 = qual1[u:u+len(new_seq1)]
+            qual2 = qual2[u:u+len(new_seq2)]
+
+            assert len(new_seq1) == len(qual1)
+            assert len(new_seq2) == len(qual2)
+
+            self.fq1_writer.write(header1 + '\n')
+            self.fq2_writer.write(header2 + '\n')
+            self.fq1_writer.write(new_seq1 + '\n')
+            self.fq2_writer.write(new_seq2 + '\n')
+            self.fq1_writer.write('+\n')
+            self.fq2_writer.write('+\n')
+            self.fq1_writer.write(qual1 + '\n')
+            self.fq2_writer.write(qual2 + '\n')
+
+        self.close_files()
+
+        self.logger.info(f'''\
+{self.fq1} ({total_1:,} bp) -> ({remain_1:,} bp = {remain_1/total_1:.2f}%) {self.out_fq1}
+{self.fq2} ({total_2:,} bp) -> ({remain_2:,} bp = {remain_2/total_2:.2f}%) {self.out_fq2}''')
+
+        self.gzip_output()
+
+        return self.out_fq1, self.out_fq2
+
+    def open_files(self):
+        self.out_fq1 = f'{self.workdir}/umi_adapter_removed_R1.fastq'
+        self.out_fq2 = f'{self.workdir}/umi_adapter_removed_R2.fastq'
+        self.fq1_reader = gzip.open(self.fq1, 'rt') if self.fq1.endswith('.gz') else open(self.fq1, 'r')
+        self.fq2_reader = gzip.open(self.fq2, 'rt') if self.fq2.endswith('.gz') else open(self.fq2, 'r')
+        self.fq1_writer = open(self.out_fq1, 'w')
+        self.fq2_writer = open(self.out_fq2, 'w')
+
+    def close_files(self):
+        self.fq1_reader.close()
+        self.fq2_reader.close()
+        self.fq1_writer.close()
+        self.fq2_writer.close()
+
+    def gzip_output(self):
+        if self.gz:
+            self.call(self.out_fq1)
+            self.call(self.out_fq2)
+            self.out_fq1 += '.gz'
+            self.out_fq2 += '.gz'
+
+
+def strip_mate_3prime_umi(read: str, mate: str) -> str:
+    mate_rc = rev_comp(mate)
+    pos = mate_rc.find(read[:15])  # 4^15 = 1,073,741,824 should be specific enough
+    return mate[:-pos] if pos > 0 else mate
--- test/test_trimming.py
+++ test/test_trimming.py
@@ -1,4 +1,4 @@
-from somatic_pipeline.trimming import Trimming
+from somatic_pipeline.trimming import Trimming, RemoveUmiAndAdapter, strip_mate_3prime_umi
 from .setup import TestCase
 
 
@@ -23,3 +23,47 @@ class TestTrimming(TestCase):
         self.assertFileExists(f'{self.workdir}/tumor.2_val_2.fq.gz', tumor_fq2)
         self.assertIsNone(normal_fq1)
         self.assertIsNone(normal_fq2)
+
+
+class TestRemoveUmiAndAdapter(TestCase):
+
+    def setUp(self):
+        self.set_up(py_path=__file__)
+
+    def tearDown(self):
+        self.tear_down()
+
+    def test_main(self):
+        fq1, fq2 = RemoveUmiAndAdapter(self.settings).main(
+            fq1=f'{self.indir}/tumor.1.fq.gz',
+            fq2=f'{self.indir}/tumor.2.fq.gz',
+            umi_length=7,
+            gz=False
+        )
+        self.assertFileExists(f'{self.workdir}/umi_adapter_removed_R1.fastq', fq1)
+        self.assertFileExists(f'{self.workdir}/umi_adapter_removed_R2.fastq', fq2)
+
+
+class TestFunctions(TestCase):
+
+    def test_has_umi(self):
+        actual = strip_mate_3prime_umi(
+            read='A' * 20,
+            mate='T' * 20 + 'ACGTACGT'
+        )
+        self.assertEqual('T' * 20, actual)
+
+    def test_no_umi(self):
+        actual = strip_mate_3prime_umi(
+            read='A' * 20,
+            mate='T' * 20
+        )
+        self.assertEqual('T' * 20, actual)
+
+    def test_no_overlap(self):
+        actual = strip_mate_3prime_umi(
+            read='A' * 20,
+            mate='G' * 20
+        )
+        self.assertEqual('G' * 20, actual)
+
